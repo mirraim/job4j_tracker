122ef523badbb501dccb132f26bf727c438959a7
Maria Pankratova <mirraim@gmail.com>
2020-11-29 00:00:00 +0500

    8. Что такое валидация? [#399416]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -72,12 +72,13 @@ public class Tracker {
      */
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
-        } else {
+        boolean rsl = index != -1;
+        if (rsl) {
             item.setId(id);
             items[index] = item;
             return true;
+        } else {
+            return false;
         }
     }
 
@@ -104,13 +105,14 @@ public class Tracker {
      */
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
-        } else {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index);
             items[size - 1] = null;
             size--;
             return true;
+        } else {
+            return false;
         }
     }
 }
