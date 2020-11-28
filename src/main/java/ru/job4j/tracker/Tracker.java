d442329c5ed4a53fe38abccf6cbb548f611de7b5
Maria Pankratova <mirraim@gmail.com>
2020-11-28 18:42:14 +0500

    5. Tracker - хранилище [#399419]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,29 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    /**
+     * добавляет заявку, переданную в аргументах в массив заявок items
+     * @param item заявка
+     * @return item
+     */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /**
+     * проверяет в цикле все элементы массива items, сравнивая id с аргументом int id
+     * и возвращает найденный Item. Если Item не найден - возвращает null
+     * @param id - уникальный номер
+     * @return Item или null
+     */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -21,5 +34,40 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * возвращает копию массива items без null элементов (без пустых ячеек)
+     * @return Item[] без пустых ячеек
+     */
+    public Item[] findAll() {
+        int count = 0;
+        Item[] itemsWithoutNull = new Item[size];
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[count] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, count);
+    }
+
+    /**
+     * проверяет в цикле все элементы массива items,
+     * сравнивая name (используя метод getName класса Item) с аргументом метода String key.
+     * Элементы, у которых совпадает name, копирует в результирующий массив и возвращает его
+     * @param key - имя
+     * @return Item[] с именами, равными key
+     */
+    public Item[] findByName(String key) {
+        int count = 0;
+        Item[] names = new Item[size];
+        for (int i = 0; i < size; i++) {
+           if (key.equals(items[i].getName())) {
+               names[count] = items[i];
+               count++;
+           }
+        }
+        return Arrays.copyOf(names, count);
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
+
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
@@ -13,5 +14,46 @@ public class TrackerTest {
         tracker.add(item);
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void findItemWhenSomeID() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item(1, "one");
+        Item item2 = new Item(2, "two");
+        Item item3 = new Item(3, "three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item result = tracker.findById(2);
+        assertThat(result, is(item2));
+    }
+
+    @Test
+    public void findAllTest() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item(1, "one");
+        Item item2 = new Item(2, "two");
+        Item item3 = new Item(3, "three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] result = tracker.findAll();
+        Item[] expected = {item1, item2, item3};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void findByNameTest() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item(1, "one");
+        Item item2 = new Item(2, "two");
+        Item item3 = new Item(3, "three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] result = tracker.findByName("three");
+        Item[] expected = {item3};
+        assertThat(result, is(expected));
     }
 }
