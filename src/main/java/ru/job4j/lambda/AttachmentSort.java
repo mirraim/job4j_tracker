c281a85f0dd32e269ed1b8da877b4ea91e71be58
Maria Pankratova <mirraim@gmail.com>
2021-01-11 23:30:12 +0500

    2.2. Лямбда блок [#399143] - добавлена отладочная информация

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,11 +11,16 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
+        Comparator<Attachment> comparator = (left, right) -> {
+            System.out.println("compare - " + left.getSize() + " : " + right.getSize());
+            return left.getSize() - right.getSize();
+        };
         attachments.sort(comparator);
         System.out.println(attachments);
-        Comparator<Attachment> objComparator = (left, right) ->
-                right.getName().compareTo(left.getName());
+        Comparator<Attachment> objComparator = (left, right) -> {
+            System.out.println("compare - " + left.getName() + " : " + right.getName());
+            return right.getName().compareTo(left.getName());
+        };
         attachments.sort(objComparator);
         System.out.println(attachments);
     }
