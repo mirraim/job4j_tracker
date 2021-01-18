2a9b450b3db4fedf4b49eabe2a3e327b9deebf87
Maria Pankratova <mirraim@gmail.com>
2021-01-18 21:19:02 +0500

    2.0. Встроенные функциональные интерфейсы. [#399144]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -2,22 +2,33 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class SearchAtt {
     public static List<Attachment> filterSize(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getSize() > 100) {
-                rsl.add(att);
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getSize() > 100;
             }
-        }
-        return rsl;
+        };
+        return filter(list, predicate);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getName().contains("bug");
+            }
+        };
+        return filter(list, predicate);
+    }
+
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> p) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            if (att.getName().contains("bug")) {
+            if (p.test(att)) {
                 rsl.add(att);
             }
         }
