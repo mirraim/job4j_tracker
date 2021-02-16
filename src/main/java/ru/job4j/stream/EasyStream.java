318d1ae86b24f65e3f6d81264afa06aa13fde567
mirraim <mirraim@gmail.com>
2021-02-16 12:55:51 +0500

    EasyStream [#401629] - замена геттера и сеттера на приватный конструктор

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -9,41 +9,36 @@ public class EasyStream {
 
     private List<Integer> list;
 
-    public List<Integer> getList() {
-        return list;
-    }
-
-    public void setList(List<Integer> list) {
+    private EasyStream(List<Integer> list) {
         this.list = list;
     }
 
+    private EasyStream() {
+    }
+
     public static EasyStream of(List<Integer> source) {
-        EasyStream easyStream = new EasyStream();
-        easyStream.setList(source);
-        return easyStream;
+        return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
         List<Integer> rsl = new ArrayList<>();
-        for (Integer number : this.getList()) {
+        for (Integer number : this.list) {
            rsl.add(fun.apply(number));
         }
-        this.setList(rsl);
-        return this;
+        return new EasyStream(rsl);
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
         List<Integer> rsl = new ArrayList<>();
-        for (Integer number : this.getList()) {
+        for (Integer number : this.list) {
             if (fun.test(number)) {
                 rsl.add(number);
             }
         }
-        this.setList(rsl);
-        return this;
+        return new EasyStream(rsl);
     }
 
     public List<Integer> collect() {
-        return this.getList();
+        return this.list;
     }
 }
