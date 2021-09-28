2edd451b93927a12d5d700a9d0a38ab9d72e0e56
Mirraim <mirraim@gmail.com>
2021-09-28 11:00:59 +0500

    0. Что такое Mock-объект. [#6870]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.job4j.tracker.action.*;
 import ru.job4j.tracker.input.Input;
@@ -8,21 +11,58 @@ import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.output.StubOutput;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private static Connection connection;
+
+    @BeforeClass
+    public static void initConnection() {
+        try (InputStream in = SqlTrackerTest.class.getClassLoader()
+                .getResourceAsStream("test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @After
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
+            statement.execute();
+        }
+    }
+
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
         );
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         Output out = new ConsoleOutput();
         List<UserAction> actions = new ArrayList<>();
         actions.add(new CreateAction(out));
@@ -35,8 +75,7 @@ public class StartUITest {
 
     @Test
     public void whenReplaceItem() {
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         Output output = new ConsoleOutput();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
@@ -54,8 +93,7 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItem() {
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         Output output = new ConsoleOutput();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
@@ -71,8 +109,7 @@ public class StartUITest {
 
     @Test
     public void whenFindAllItems() {
-       Store tracker = new SqlTracker();
-       tracker.init();
+        Store tracker = new SqlTracker(connection);
        Item item = tracker.add(new Item("New Item"));
        Input in = new StubInput(new String[]{"0", "1"});
        Output output = new ConsoleOutput();
@@ -89,8 +126,7 @@ public class StartUITest {
 
     @Test
     public void whenFindItemByName() {
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         Item item = tracker.add(new Item("New Item"));
         Input in = new StubInput(new String[]{"0", "New Item", "1"});
         Output output = new ConsoleOutput();
@@ -107,8 +143,7 @@ public class StartUITest {
 
     @Test
     public void whenFindItemById() {
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         Item first = tracker.add(new Item("New Item"));
         Item second = tracker.add(new Item("One more Item"));
         Input in = new StubInput(new String[]{"0", String.valueOf(second.getId()), "1"});
@@ -131,8 +166,7 @@ public class StartUITest {
         Input in = new StubInput(
                 new String[] {"7", "0"}
         );
-        Store tracker = new SqlTracker();
-        tracker.init();
+        Store tracker = new SqlTracker(connection);
         List<UserAction> actions = new ArrayList<>();
         actions.add(new ExitAction());
         new StartUI(out).init(in, tracker, actions);
