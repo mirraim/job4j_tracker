f2946e5735a094aef9a165ff6b0884af977bf086
Mirraim <mirraim@gmail.com>
2021-10-16 17:49:49 +0500

    DATABASECHANGELOGLOCK

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,11 @@
-update DATABASECHANGELOGLOCK set LOCKED=false, LOCKGRANTED=null, LOCKEDBY=null where ID=1;
+create table if not exists DATABASECHANGELOGLOCK (
+    ID int,
+    LOCKED int,
+    LOCKGRANTED DATE,
+    LOCKEDBY VARCHAR(255)
+);
+
+UPDATE DATABASECHANGELOGLOCK SET LOCKED=0, LOCKGRANTED=null, LOCKEDBY=null where ID=1;
 
 create table if not exists items (
     id serial primary key,
