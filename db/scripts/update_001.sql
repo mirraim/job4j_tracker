d0658afa8f3913cf7ae7349c8491210350402862
Mirraim <mirraim@gmail.com>
2021-10-17 21:14:48 +0500

    коррекция db.url  и версии jaCoCo

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,12 +1,3 @@
-create table if not exists DATABASECHANGELOGLOCK (
-    ID int,
-    LOCKED int,
-    LOCKGRANTED DATE,
-    LOCKEDBY VARCHAR(255)
-);
-
-UPDATE DATABASECHANGELOGLOCK SET LOCKED=0, LOCKGRANTED=null, LOCKEDBY=null where ID=1;
-
 create table if not exists items (
     id serial primary key,
     name varchar(2000),
